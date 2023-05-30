472912d31743e77c7a1fbbe23afcb637a57a7493
joaovieitas420 <joao.vieitas2@gmail.com>
2023-05-30 11:58:19 +0100

    Repository

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -31,6 +31,7 @@ namespace SuperShop
             });
 
             services.AddTransient<SeedDb>();
+            services.AddScoped<IRepository, Repository>();
 
             services.AddControllersWithViews();
         }
